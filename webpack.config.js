840b7d4af2237396fb8960a88c67641868f76565
Bagchi <rupsha.bagchi@accenture.com>
2017-11-17 13:41:24 +0200

    adding eslint

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,14 @@ module.exports = {
       test: /\.jsx?$/,
       use: ['babel-loader'],
       include: path.join(__dirname, 'src')
-    }]
+    }],
+    loaders: [{
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        loader: 'eslint',
+        exclude: /node_modules/
+      }]
   }
 };
