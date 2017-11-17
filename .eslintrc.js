b9aa21a69e295cfb685cc7ffe2414335effaf35c
Bagchi <rupsha.bagchi@accenture.com>
2017-11-17 15:21:51 +0200

    adding jest and enzyme tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
         'jest': true
     },
     "extends": "eslint:recommended",
+    "parser": "babel-eslint",
     "parserOptions": {
         "ecmaFeatures": {
             "experimentalObjectRestSpread": true,
--- src/test/TodoModel.test.js
+++ src/test/TodoModel.test.js
@@ -1,10 +1,10 @@
-import {TodoStore} from "../src/components/Todo"
+import {TodoListModel} from "../models/TodoListModel"
 
 describe("TodoStore", () => {
     it("creates new todos", () => {
-        const store = new TodoStore
-        store.createTodo("todo1")
-        store.createTodo("todo2")
+        const store = new TodoListModel
+        store.addTodo("todo1")
+        store.addTodo("todo2")
         expect(store.todos.length).toBe(2)
         expect(store.todos[0].value).toBe("todo1")
         expect(store.todos[1].value).toBe("todo2")
